338c8fcb85e74d4e8bc0e76ba86155fd7836e4be
IsraelHer666 <bisrael240@gmail.com>
2023-09-26 15:36:38 -0600

    Agregando carpetas necesarias

--- NodeJs/Index.js
+++
@@ -1,6 +0,0 @@
-const fs=require("fs");
-fs.writeFile("messi2.txt","Este es mi contenido",(err) => { //El metodo writeFile nos permite crear un archivo
-  //writeFile("nombre del archivo,contenido, callback")lo recibe en string
-  if (err) throw err;
-  console.log('successfully deleted /tmp/hello');
-});
